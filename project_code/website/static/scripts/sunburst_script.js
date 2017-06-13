94a54919405dd1c193d5ba7c0b705f305ae0ab66
rickbeeloo <r.beeloo@outlook.com>
2017-06-13 11:11:46 +0200

    hand edit

--- project_code/website/static/scripts/sunburst_script.js
+++ project_code/website/static/scripts/sunburst_script.js
@@ -215,6 +215,10 @@ function fillTablex(d) {
 
 
 function create_table(head, data, need_textbox) {
+	head.push("PMID's");
+	data.push(kut_koen());
+	need_textbox.push("PMID's");
+	
   var i=0, rowEl=null,
 	  tableEl = document.createElement("table");
 	  for (i=0; i < head.length; i++) {
@@ -256,6 +260,22 @@ function get_data(table, values,  name, keyword_column) {
 			return table_dat;
 		}
 
+
+function kut_koen() {
+	ids = [];
+	aantal =10;
+
+
+	for (j=0; j<aantal; j++ ) {
+		id = "PMID";
+		for (i=0; i < 8; i++) {
+    		random_numb = Math.floor((Math.random() * 10) + 1);
+    		id += random_numb + "&#13;&#10;";
+    }
+    ids.push(id)
+	}
+	return ids
+}
 
 
 
